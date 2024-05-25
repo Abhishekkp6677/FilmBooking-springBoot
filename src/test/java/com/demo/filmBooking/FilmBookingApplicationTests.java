1aca2a92e919e90329ab1220111fbac5d2261dff
Abhishek k p <abhishek.k.p.6677@gmail.com>
2024-05-25 18:57:29 +0530

    initial commit

---
+++ src/test/java/com/demo/filmBooking/FilmBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.filmBooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilmBookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
